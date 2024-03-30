36bfe60502af970d8eabe2372d7ff0506ba3197b
vinayakjain <vinaaykjain388@gmail.com>
2024-03-30 10:04:52 +0530

    fix again

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.DOMAIN);
+  res.header('Access-Control-Allow-Origin', 'https://metagramapp.netlify.app');
   res.header(
     'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization,  X-PINGOTHER'
   );
