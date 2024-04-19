2071c39cc8fc7d9a2873e19baa53de3d9efb1e42
vinayakjain <vinaaykjain388@gmail.com>
2024-04-19 09:27:49 +0530

    fix

--- controllers/user.js
+++ controllers/user.js
@@ -39,7 +39,7 @@ exports.register = async (req, res) => {
             sameSite:'none',
             secure:true,
             httpOnly:true,
-            domain:".netlify.app",
+            domain:".onrender.com",
            
           
         })
@@ -101,7 +101,7 @@ exports.login = async (req, res, next) => {
             sameSite:'none',
             secure:true,
             httpOnly:true,
-            domain:".netlify.app",
+            domain:".onrender.com",
         
             
           
@@ -189,7 +189,7 @@ exports.logout = async (req, res, next) => {
             sameSite:'none',
             secure:true,
             httpOnly:true,
-            domain:".netlify.app",
+            domain:".onrender.com",
            
         }).json({
             success: true,
